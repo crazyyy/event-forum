2d50e7e9558efe4f828f403eaa3d078a951d380b
crazyyy <crazyyy@gmail.com>
2015-06-04 10:44:09 +0300

    webp fix

--- html/index2.php
+++ html/index2.php
@@ -13,10 +13,6 @@
   <!-- css + javascript -->
   <link href="css/main.css" rel="stylesheet">
   <script async type="text/javascript" src="///cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
-  <script>(function(){var WebP=new Image();WebP.onload=WebP.onerror=function(){
-if(WebP.height!=2){var sc=document.createElement('script');sc.type='text/javascript';sc.async=false;
-var s=document.getElementsByTagName('script')[0];sc.src='js/webpjs-0.0.2.min.js';s.parentNode.insertBefore(sc,s);}};
-WebP.src='data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';})();</script>
 </head>
 <body>
 <!-- wrapper -->
